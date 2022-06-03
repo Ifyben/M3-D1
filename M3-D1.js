9e594b16027205625c865dadeeb145df5364f100
Beauty Azubuike <102667250+Ifyben@users.noreply.github.com>
2022-06-03 22:43:57 +0200

    Exercise 17

--- M3-D1.js
+++ M3-D1.js
@@ -223,6 +223,15 @@ console.log(getLargestInt([2, 5, 6, 7]))
 /* 17. Create a function to calculate the sum of two integers (passed as parameters). 
     If the sum is in the 50-80 range, return `65`, otherwise, return `80`. */
 
+    const checkSumRange = function(a, b) {
+        if(a + b > 50 && a + b < 80) {
+            return 65;
+        } else {
+            return 80;
+        }
+    };
+    console.log(checkSumRange(2, 2));
+    console.log(checkSumRange(30, 30));
 
 /* 18. Create a function to convert a number (passed as a parameter) into a string, basing yourself on this example: 
     The number has 3 as a factor ⇒ return `Diego`
